2db4f28f3cb342bb3c1e0104daeb9fe8494a163c
Frede <frederikkkoch@web.de>
2024-03-19 19:24:20 +0100

    added css and javascript

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-let story = [""];
+document.addEventListener("DOMContentLoaded", function () {
+  let text = ["", "xRubiks Story Game", "Lol"];
+  let clicks = 0;
+  let nextButton = document.getElementById("Next-Button");
+  let story = document.getElementById("story");
+  let h1 = document.getElementById("h1");
+
+  nextButton.addEventListener("click", function () {
+    clicks++;
+    story.innerHTML = text[clicks];
+    hideTitle(); // Aufruf der Funktion zum Verstecken des Titels nach jedem Klick
+  });
+
+  function hideTitle() {
+    if (clicks == 1) {
+      h1.style.display = "none"; // Korrekte Referenzierung des h1-Elements
+    }
+  }
+});
