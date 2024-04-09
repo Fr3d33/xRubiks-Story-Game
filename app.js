a2fc7cab23fbc3de9ed1c2453ffe6caa178b366d
Fr3d33 <frederikkkoch@web.de>
2024-04-09 23:28:22 +0200

    added voicelines 3-6

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   let clicks = 0;
   let audio1 = new Audio("/voicelines/voiceline1.mp3");
   let audio2 = new Audio("/voicelines/voiceline2.mp3");
+  let audio3 = new Audio("/voicelines/voiceline3.mp3");
+  let audio4 = new Audio("/voicelines/voiceline3.mp4");
+  let audio5 = new Audio("/voicelines/voiceline3.mp5");
+  let audio6 = new Audio("/voicelines/voiceline3.mp6");
   let text = [];
   let nextButton = document.getElementById("Next-Button");
   let story = document.getElementById("story");
@@ -42,8 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (clicks == 0) {
       audio1.pause();
       audio1.currentTime = 0;
-      audio2.pause();
-      audio2.currentTime = 0;
     }
     if (clicks == 1) {
       audio2.pause();
@@ -51,9 +53,27 @@ document.addEventListener("DOMContentLoaded", function () {
       audio1.play();
     }
     if (clicks == 2) {
-      audio1.pause();
-      audio1.currentTime = 0;
+      audio3.pause();
+      audio3.currentTime = 0;
       audio2.play();
+    }
+    if (clicks == 3) {
+      audio4.pause();
+      audio4.currentTime = 0;
+      audio3.play();
+    }
+    if (clicks == 4) {
+      audio5.pause();
+      audio5.currentTime = 0;
+      audio4.play();
+    }
+    if (clicks == 5) {
+      audio6.pause();
+      audio6.currentTime = 0;
+      audio5.play();
+    }
+    if (clicks == 6) {
+      audio6.play();
     }
   }
 
